bcae7592fd08bd7674ef565faaccf2cef05c206e
alvan <alvan.nauval@gmail.com>
2023-03-29 21:14:34 +0700

    inverseKinematics and keyboardRoutine

--- omni_control.py
+++ omni_control.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import time
 import keyboard
 import numpy as np
@@ -40,6 +42,59 @@ def setRobotMotion(clientID, motors, velocity):
     sim.simxSetJointTargetVelocity(clientID, motors[3], velocity[3], sim.simx_opmode_oneshot)
 
 
+def inverseKinematics(local_speed):
+
+    L = 0.5 # distance to wheel
+    alpha_1 = np.deg2rad(-135)
+    alpha_2 = np.deg2rad(-45)
+    alpha_3 = np.deg2rad(45)
+    alpha_4 = np.deg2rad(135)
+
+    V = np.array([[local_speed[0]],
+                  [local_speed[1]],
+                  [local_speed[2]]])
+
+    A = np.array([[-np.sin(alpha_1), np.cos(alpha_1), L],
+                  [-np.sin(alpha_2), np.cos(alpha_2), L],
+                  [-np.sin(alpha_3), np.cos(alpha_3), L],
+                  [-np.sin(alpha_4), np.cos(alpha_4), L]])
+
+    Phi = np.matmul(A, V)
+
+    print("Phi0 = {} Phi1 = {} Phi2 = {} Phi3 = {}".format(Phi[0], Phi[1], Phi[2], Phi[3]))    
+    return float(Phi[0]), float(Phi[1]), float(Phi[2]), float(Phi[3])
+
+def keyboardRoutine():
+
+    localSpeed = [0, 0, 0]
+    # Horizontal and Vertical
+    if keyboard.is_pressed('w'):
+        localSpeed = [0, 4, 0]
+    elif keyboard.is_pressed('s'):
+        localSpeed = [0, -4, 0]
+    elif keyboard.is_pressed('a'):
+        localSpeed = [-4, 0, 0]
+    elif keyboard.is_pressed('d'):
+        localSpeed = [4, 0, 0]
+
+    # Diagonal
+    elif keyboard.is_pressed('q'):
+        localSpeed = [-4, 4, 0]
+    elif keyboard.is_pressed('e'):
+        localSpeed = [4, 4, 0]
+    elif keyboard.is_pressed('z'):
+        localSpeed = [-4, -4, 0]
+    elif keyboard.is_pressed('c'):
+        localSpeed = [4, -4, 0]
+
+    # Rotation
+    elif keyboard.is_pressed('r'):
+        localSpeed = [0, 0, -4]
+    elif keyboard.is_pressed('t'):
+        localSpeed = [0, 0, 4]
+
+    return localSpeed
+
 # === Main Program ===
 # --------------------
 print("Program started")
@@ -59,16 +114,32 @@ for i in range(2):
 # --- Get initial Pose ---
 robotPose = getObjectPose(client_id, robotHandler, block=True)
 
-
+# --- Initial Param ---
 samp_time = 0.1
 n = 1
 time_start = time.time()
 
+wheelsPhi = [0, 0, 0, 0]
+localSpeed = [0, 0, 0]
+mode = 1
+
 while True:
     t_now = time.time() - time_start
     if t_now >= samp_time*n:
-        setRobotMotion(client_id, wheelHandler, [3, 3, 3, 3])
-        print("ya")
+        # update time
+        n += 1
+
+        robotPose = getObjectPose(client_id, robotHandler)
+
+        if(mode ==1):
+            localSpeed = keyboardRoutine()
+            wheelsPhi = inverseKinematics(localSpeed)
+
+        setRobotMotion(client_id, wheelHandler, wheelsPhi)
+        
+        print("Wheels Phi: {:.2f}, {:.2f}, {:.2f}, {:.2f}".format(wheelsPhi[0], wheelsPhi[1], wheelsPhi[2], wheelsPhi[3]))
+        print("Time = {} Robot Pose: {:.2f}, {:.2f}, {:.2f}\n".format(round(t_now,2), robotPose[0], robotPose[1], robotPose[2]*(180/np.pi)))
+        # print("ya")
     
 # --- End of Simulation ---
 sim.simxFinish(client_id)
